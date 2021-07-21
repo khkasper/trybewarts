720c52cec82ccc2e43d17163c3c562c9b1ee1287
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-07-21 19:13:28 -0300

    ate o 19 html e js OK, CSS nao

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.querySelector('.email');
 const senha = document.querySelector('.senha');
 const btnlogin = document.querySelector('.submit-botao-login');
+const checkAgree = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
 
 function alertLogin() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -11,3 +13,17 @@ function alertLogin() {
 }
 
 btnlogin.addEventListener('click', alertLogin);
+
+btnSubmit.disabled = true;
+function checkSelected(event) {
+  // eslint-disable-next-line sonarjs/no-duplicate-string
+  if (event.target.classList.contains('botao-ativado')) {
+    event.target.classList.remove('botao-ativado');
+    btnSubmit.disabled = true;
+  } else {
+    event.target.classList.add('botao-ativado');
+    btnSubmit.disabled = false;
+  }
+}
+
+checkAgree.addEventListener('click', checkSelected);
